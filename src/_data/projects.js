3e603010e681ba7abfa7239470b80b844bada8a4
Deepak <dee-kap@users.noreply.github.co>
2025-09-25 14:49:45 +1000

    Basic carousel working

--- src/_data/projects.js
+++ src/_data/projects.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const basePath = "static";
+const basePath = "static/projects";
 
 function getImages(projectName) {
   const folder = path.join(__dirname, "..", "..", basePath, projectName);
@@ -13,8 +13,19 @@ function getImages(projectName) {
     .map((file) => `/${basePath}/${projectName}/${file}`);
 }
 
-module.exports = [
-  { name: "p1", images: getImages("p1") },
-  { name: "p2", images: getImages("p2") },
-  { name: "p3", images: getImages("p3") },
-];
+function getProjects() {
+  const projectsFolder = path.join(__dirname, "..", "..", basePath);
+  if (!fs.existsSync(projectsFolder)) return [];
+
+  return fs
+    .readdirSync(projectsFolder)
+    .filter((name) =>
+      fs.statSync(path.join(projectsFolder, name)).isDirectory()
+    )
+    .map((name) => ({
+      name,
+      images: getImages(name),
+    }));
+}
+
+module.exports = getProjects();
