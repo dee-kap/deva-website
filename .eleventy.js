f140008da6ae5e3bcf955376ad13ab552f6e9ae6
Deepak <dee-kap@users.noreply.github.co>
2025-10-03 19:07:30 +1000

    dist

--- .eleventy.js
+++ .eleventy.js
@@ -11,7 +11,7 @@ module.exports = function (eleventyConfig) {
     dir: {
       input: "src",
       includes: "_includes",
-      output: "_site",
+      output: "dist",
     },
     markdownTemplateEngine: "njk",
   };
